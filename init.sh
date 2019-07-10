a280e1860ab8002e4efd35edd24ab7d3f49dc99f
Abner Chou <hi@abnerchou.me>
2019-07-09 23:31:32 -0400

    Add more code

--- init.sh
+++ init.sh
@@ -20,3 +20,24 @@ export TRAIN_DATA=$(pwd)/data/adult.data.csv
 export EVAL_DATA=$(pwd)/data/adult.test.csv
 
 head data/adult.data.csv
+
+#Install Dependencies
+pip install --user -r ../requirements.txt
+
+export MODEL_DIR=output
+
+gcloud ai-platform local train \
+       --module-name trainer.task \
+       --package-path trainer/ \
+       --job-dir $MODEL_DIR \
+       -- \
+       --train-files $TRAIN_DATA \
+       --eval-files $EVAL_DATA \
+       --train-steps 1000 \
+       --eval-steps 100
+
+#Open TensorBoard
+# tensorboard --logdir=$MODEL_DIR --port=8080
+
+export TIME_STAMP=$(ls output/export/census)
+echo $TIME_STAMP
