e36b559809bcba4ad12f9620b2a25089a4fbadb1
Abner Chou <hi@abnerchou.me>
2019-07-09 23:24:07 -0400

    Update line ending

--- init.sh
+++ init.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 
 gcloud auth list
+
 gcloud config list project
 
 pip install --user --upgrade tensorflow
+
 python -c "import tensorflow as tf; print('TensorFlow version {} is installed.'.format(tf.VERSION))"
+
+git clone https://github.com/GoogleCloudPlatform/cloudml-samples.git
+
+cd cloudml-samples/census/estimator
+
+mkdir data
+
+gsutil -m cp gs://cloud-samples-data/ml-engine/census/data/* data/
+
+export TRAIN_DATA=$(pwd)/data/adult.data.csv
+export EVAL_DATA=$(pwd)/data/adult.test.csv
+
+head data/adult.data.csv
